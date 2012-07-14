e6ea4ed35d5877935ffc931da00dcd9839d52fda
Paul <paul@mac.com>
2012-07-14 13:06:42 +0300

    ready for assignment 2

--- Calculator/CalculatorBrain.h
+++ Calculator/CalculatorBrain.h
@@ -13,4 +13,10 @@
 - (void)pushOperand:(double)operand;
 - (double)performOperation:(NSString*) operation;
 
+@property (readonly) id program;
+
++ (double)runProgram:(id)program; // nu schimba implementarea
+
++ (NSString *)descriptionOfProgram:(id)program; // nu schimba implementarea
+
 @end
--- Calculator/CalculatorBrain.m
+++ Calculator/CalculatorBrain.m
@@ -10,7 +10,7 @@
 
 @interface CalculatorBrain()
 // aici definim chestii private
-@property (nonatomic, strong) NSMutableArray *operandStack;
+@property (nonatomic, strong) NSMutableArray *programStack;
 
 @end
 
@@ -18,82 +18,109 @@
 
 @implementation CalculatorBrain
 // trebuie sa implementam proprietarea de mai sus, cu getter si setter
-@synthesize operandStack = _operandStack;
+@synthesize programStack = _programStack;
 
-- (NSMutableArray *)operandStack
+- (NSMutableArray *)programStack
 {
-    if (_operandStack == nil) _operandStack = [[NSMutableArray alloc] init ];//aloc,ca
+    if (_programStack == nil) _programStack = [[NSMutableArray alloc] init ];//aloc,ca
     //sa ma asigur ca nu e niciodata nill
-    return _operandStack;
+    return _programStack;
 }
-- (void)setOperandStack:(NSMutableArray *)operandStack
+- (void)setProgramStack:(NSMutableArray *)programStack
 {
-    _operandStack = operandStack;
+    _programStack = programStack;
 }
-- (double) popOperand 
-{
-    NSNumber *operandObject = [self.operandStack lastObject ];
-    //atentie sa nu crasuim programul, incarcand sa scoatem dintrun array gol
-    if (operandObject != nil) [ self.operandStack removeLastObject ];
-    return [ operandObject doubleValue ];
-}
+//- (double) popOperand 
+//{
+//    NSNumber *operandObject = [self.programStack lastObject ];
+//    //atentie sa nu crasuim programul, incarcand sa scoatem dintrun array gol
+//    if (operandObject != nil) [ self.programStack removeLastObject ];
+//    return [ operandObject doubleValue ];
+//}
 - (void)pushOperand:(double)operand{
     NSNumber * operandObject = [ NSNumber numberWithDouble:operand ];
-    [ self.operandStack addObject:operandObject ]; //nu e bun cu operand, ca nu e obiect, asa ca tre sa ii fac un wrapper
+    [ self.programStack addObject:operandObject ]; //nu e bun cu operand, ca nu e obiect, asa ca tre sa ii fac un wrapper
     // trebuie sa ma asigur in getter ca nu fac operatii pe operandStack cand e nil, ca altfel nu se intampla nimica
     
 }
-- (double)performOperation:(NSString*) operation{
-    double result = 0;
-    //calculam rezultat in functie de operatie
-    if ([operation isEqualToString:@"+" ]) {
-        return [ self popOperand ] + [ self popOperand ];
-    }
-    // si acum trimit un MESAJ LA UN CONSTANT STRING
-    else if ([ @"*" isEqualToString:operation])
-    {
-        return [ self popOperand ] * [ self popOperand ];
-    }
-    else if ([operation isEqualToString:@"-" ]) 
-    {
-        double secondOperand = [ self popOperand ];
-        return [ self popOperand ] - secondOperand;
-    }
-    else if ([ operation isEqualToString:@"/"])
-    {
-        double divide = [ self popOperand ];
-        if (divide) return [self popOperand ] / divide; // evitam impartirea la 0
-    } 
-    else if ([operation isEqualToString:@"sin"])
-    {
-        return sin([self popOperand ]);
-        
-    }
-    else if ([operation isEqualToString:@"cons"])
-    {
-        return cos([self popOperand ]);
-        
-    }
-    else if ([operation isEqualToString:@"sqrt"])
-    {
-        double numb = [ self popOperand ];
-        if (numb > 0) {
-            return sqrt(numb);
-        }
-        else {
-            return 0; //ca asa vrem noi
-        }
-    } else if ([operation isEqualToString:@"pi"])
-    {
-        // nu iau nimic de pe stiva
-        return M_PI;
-    }
-    //[self pushOperand:result ];
-    // punem inapoi pe stiva
-    [ self pushOperand:result ];
-    
-    return result;
-    
+// nu @syntesize program, ci implementez doar getterul
+- (id) program
+{
+    return [ self.programStack copy ]; //e un snapshot, o copie
 }
+
++ (NSString *)descriptionOfProgram:(id)program
+{
+    return @"Implement in assignment 2";
+}
+
++ (double)popOperandOffStack:(NSMutableArray *)stack
+{
+    double result = 0;
+    
+    id topOfStack = [ stack lastObject ];
+    if (topOfStack) [stack removeLastObject ];//rezultatul poate fi NSNumber sau NSString
+    if ([topOfStack isKindOfClass:[NSNumber class]]) {
+        result = [topOfStack doubleValue];
+    }
+    else if ([topOfStack isKindOfClass:[NSString class]]) {
+        NSString *operation = topOfStack; // asign de id la string, si compilerul nu are nici o problema. eu tre sa stiu ce fac
+        
+        if ([operation isEqualToString:@"+" ]) {
+            return [ self popOperandOffStack:stack ] + [ self popOperandOffStack:stack ];
+        } // si acum trimit un MESAJ LA UN CONSTANT STRING
+        else if ([ @"*" isEqualToString:operation]) {
+            return [ self popOperandOffStack:stack ] * [ self popOperandOffStack:stack ];
+        }
+        else if ([operation isEqualToString:@"-" ]) {
+            double secondOperand = [ self popOperandOffStack:stack ];
+            return [ self popOperandOffStack:stack ] - secondOperand;
+        }
+        else if ([ operation isEqualToString:@"/"]){
+            double divide = [ self popOperandOffStack:stack ];
+            if (divide) return [self popOperandOffStack:stack ] / divide; // evitam impartirea la 0
+        } 
+        else if ([operation isEqualToString:@"sin"]){
+            return sin([self popOperandOffStack:stack ]);
+            
+        }
+        else if ([operation isEqualToString:@"cons"]){
+            return cos([self popOperandOffStack:stack ]);
+            
+        }
+        else if ([operation isEqualToString:@"sqrt"]){
+            double numb = [ self popOperandOffStack:stack ];
+            if (numb > 0) {
+                return sqrt(numb);
+            }
+            else {
+                return 0; //ca asa vrem noi
+            }
+        } else if ([operation isEqualToString:@"pi"]) {
+            // nu iau nimic de pe stiva
+            return M_PI;
+        }
+
+    }
+    return result;
+}
++ (double)runProgram:(id)program
+{
+    NSMutableArray *stack;
+    if ([program isKindOfClass:[NSArray class]]){
+        stack = [program mutableCopy]; // program e ID
+    }
+    return [self popOperandOffStack:stack];
+}
+
+- (double)performOperation:(NSString*) operation{
+    
+    [ self.programStack addObject:operation ];
+    return [CalculatorBrain runProgram:self.program ];
+}
+    //calculam rezultat in functie de operatie
+        //[self pushOperand:result ];
+    // punem inapoi pe stiva
+    //nu mai e nevoie, din cauza implementarii recursivitatii [ self pushOperand:result ];
 
 @end
